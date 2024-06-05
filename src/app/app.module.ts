008451267c427d20bf15ef0e90079fdc31f1fdc3
danhNg08101997 <danh.nguyenthanh102@gmail.com>
2024-06-05 09:42:47 +0700

    order page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { FooterComponent } from './Components/footer/footer.component';
 import { OrderComponent } from './pages/order/order.component';
 import { ProductDetailComponent } from './pages/product-detail/product-detail.component';
 import { ProductListComponent } from './pages/product-list/product-list.component';
+import { OrderConfirmComponent } from './pages/order-confirm/order-confirm.component';
+import { LoginComponent } from './pages/login/login.component';
 
 @NgModule({
   declarations: [
@@ -15,14 +17,19 @@ import { ProductListComponent } from './pages/product-list/product-list.componen
     OrderComponent,
     ProductDetailComponent,
     ProductListComponent,
+    OrderConfirmComponent,
+    LoginComponent,
   ],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [
     // HomeComponent,
     // ProductDetailComponent,
-    OrderComponent,
+    // OrderComponent,
     // ProductListComponent,
+    // OrderConfirmComponent,
+    LoginComponent,
+
   ],
 })
 export class AppModule {}
